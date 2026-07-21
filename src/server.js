052d818f152bf368f2ac27c0f330c4fecac5e97f
Matheus Sampaio <matheusampaio098@gmail.com>
2026-07-21 10:04:37 -0300

    Backend com Resend funcionando

---
+++ src/server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+console.log(process.env.RESEND_API_KEY);
+
+const express = require("express");
+
+const {
+    enviarEmailTeste
+} = require("./services/email");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req,res) => {
+
+    try{
+
+        await enviarEmailTeste();
+
+        res.send(
+            "Email enviado com sucesso!"
+        );
+    } catch (erro) {
+
+        console.error(erro);
+
+        res.status(500).send(
+            erro.message
+        );
+
+    }
+});
+
+app.listen(3000, () => {
+
+    console.log(
+        "Servidor rodando na porta 3000"
+    );
+
+});
---
+++ src/services/email.js
@@ -0,0 +1,29 @@
+const { Resend } = require("resend");
+
+const resend = new Resend(
+    process.env.RESEND_API_KEY
+);
+
+async function enviarEmailTeste() {
+
+    const resultado = await resend.emails.send({
+
+        from: "onboarding@resend.dev",
+
+        to: ["matheusampaio098@gmail.com"],
+
+        subject: "Teste API EPI",
+
+        html: `
+            <h1>Teste realizado com sucesso!</h1>
+
+            <p>A API do sistema de EPI está funcionando.</p>
+        `
+    });
+
+    console.log(resultado);
+}
+
+module.exports = {
+    enviarEmailTeste
+};
