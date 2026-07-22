942642a88563285ddd540964f8b5be1788947f76
Matheus Sampaio <matheusampaio098@gmail.com>
2026-07-22 10:36:39 -0300

    Backend com envio de email funcionando

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 console.log(process.env.RESEND_API_KEY);
 
 const express = require("express");
+const cors = require("cors");
+
+const entregaRoutes = require("./routes/entrega");
 
 const {
     enviarEmailTeste
@@ -10,7 +13,11 @@ const {
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+app.use("/entrega", entregaRoutes);
 
 app.get("/", async (req,res) => {
 
--- src/services/email.js
+++ src/services/email.js
@@ -3,7 +3,7 @@ const { Resend } = require("resend");
 const resend = new Resend(
     process.env.RESEND_API_KEY
 );
-
+    //email de teste!
 async function enviarEmailTeste() {
 
     const resultado = await resend.emails.send({
@@ -24,6 +24,41 @@ async function enviarEmailTeste() {
     console.log(resultado);
 }
 
+    //EMAIL REAL!
+async function enviarEmailRetirada(dados) {
+    
+    const resultado = await resend.emails.send({
+
+        from: "onboarding@resend.dev",
+
+        to: ["matheusampaio098@gmail.com"],
+
+        subject: `Nova Retirada de EPI - ${dados.nome}`,
+
+        html: `
+            <h2>Nova Retirada de EPI</h2>
+            
+            <p><strong>Funcionário:</strong> ${dados.nome}</p>
+
+            <p><strong>Base Operacional:</strong> ${dados.baseoperacional}</p>
+
+            <p><strong>Data da Retirada:</strong> ${dados.dataretirada}</p>
+
+            <p><strong>Quantidade:</strong> ${dados.quantidade}</p>
+
+            <h3>Epis Retirados</h3>
+
+            <ul>
+                ${dados.episSelecionados
+                    .map(epi => `<li>${epi}</li>`)
+                    .join("")}
+            </ul>
+        `
+    });
+
+    console.log(resultado);
+}
 module.exports = {
-    enviarEmailTeste
+    enviarEmailTeste,
+    enviarEmailRetirada
 };
