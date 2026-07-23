cf36a8876869246a6e3e2c972593f97374fd42bd
Felipe Correia <felipecorreiatech@gmail.com>
2026-07-23 09:39:34 -0300

    Feature: Implementa conexão com firebase

---
+++ Server/src/services/firebase.js
@@ -0,0 +1,12 @@
+const { initializeApp, cert} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestone");
+
+const serviceAccount = require("../../src/serviceAccountKey.json");
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const db= getFirestore();
+
+module.exports = db;
