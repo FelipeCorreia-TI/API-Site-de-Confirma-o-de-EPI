aaa8934e23164cc626933ff2d2bc783a2bf0eeac
Felipe Correia <felipecorreiatech@gmail.com>
2026-07-28 11:28:51 -0300

    Fix: Arruma nome do arquivo de retiradaController.js

--- Server/src/routes/entrega.js
+++ Server/src/routes/entrega.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Importa o controller que gerencia a validação, o Firebase e o e-mail
-const { criarEntrega } = require("../controllers/entregaController");
+const { criarEntrega } = require("../controllers/retiradaController");
 
 router.post("/", criarEntrega);
 
