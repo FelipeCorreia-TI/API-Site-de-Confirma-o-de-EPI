e5eaf27739a482acd05bfe539a5f034aa29abf3d
Matheus Sampaio <matheusampaio098@gmail.com>
2026-08-04 09:45:50 -0300

    test: adiciona get em pre-reserva

--- Server/src/routes/preReserva.js
+++ Server/src/routes/preReserva.js
@@ -5,6 +5,15 @@ const {
     criarPreReserva
 } = require("../controllers/preReservaController");
 
+router.get("/", (req, res) => {
+
+    res.json({
+        sucesso: true,
+        mensagem: "Rota de pré-reserva funcionando."
+    });
+
+});
+
 router.post("/", criarPreReserva);
 
 module.exports = router;
